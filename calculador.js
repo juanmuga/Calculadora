c4bada7392c90239da825526afc4e64457b0d1bb
Juan Muga <juanmuga@Juan-Mugas-MBP.fibertel.com.ar>
2015-10-27 11:34:58 -0300

    Archivos JS CSS HTML

---
+++ calculador.js
@@ -0,0 +1,160 @@
+ var calculadora = function() {if(confirm("Quieres Sumar?")) {
+
+								var x = prompt("Cual es el Primer numero?");
+								var y = prompt("Cual es el Segundo numero?");
+								var num1 = parseInt(x); // tuve que poner esto xq el prompt me devolvia un STRING//
+ 								var num2 = parseInt(y);
+
+
+								var suma =function(num1,num2) {
+			
+								return alert(num1+num2);
+
+
+																};
+								suma(num1,num2);
+
+							} else if(confirm("Quieres Restar?")) {
+
+								var x = prompt("Cual es el Primer numero?");
+								var y = prompt("Cual es el Segundo numero?");
+								var num1 = parseInt(x); // tuve que poner esto xq el prompt me devolvia un STRING//
+ 								var num2 = parseInt(y);
+
+
+								var resta =function(num1,num2) {
+			
+								return alert(num1-num2);
+
+
+																};
+								resta (num1,num2);
+
+											} else if(confirm("Quieres Multiplicar?")) {
+
+								var x = prompt("Cual es el Primer numero?");
+								var y = prompt("Cual es el Segundo numero?");
+								var num1 = parseInt(x); // tuve que poner esto xq el prompt me devolvia un STRING//
+ 								var num2 = parseInt(y);
+
+
+								var multiplicar =function(num1,num2) {
+			
+								return alert(num1*num2);
+
+
+																};
+								multiplicar (num1,num2);
+
+											} else if(confirm("Quieres Dividir?")) {
+
+								var x = prompt("Cual es el Primer numero?");
+								var y = prompt("Cual es el Segundo numero?");
+								var num1 = parseInt(x); // tuve que poner esto xq el prompt me devolvia un STRING//
+ 								var num2 = parseInt(y);
+
+
+								var dividir =function(num1,num2) {
+			
+								return alert(num1/num2);
+
+
+																};
+								dividir (num1,num2);
+
+											} else {alert("No hay mas Opciones")};
+										};
+
+-------------------------------------------------------------------------------------------
+//calculadora con funcion switch
+
+
+
+
+
+var calculadora = function() {	
+
+								var xy = function(){
+								 x = prompt("Cual es el Primer numero?");
+								 y = prompt("Cual es el Segundo numero?");
+								 num1 = parseInt(x); 
+ 								 num2 = parseInt(y);
+ 							
+ 								 
+ 									};
+
+
+
+								switch (prompt("Que quieres hacer?")) {
+    case "sumar":
+        						
+    							xy();
+
+								var suma =function(num1,num2) {
+			
+								return alert(num1+num2);
+
+
+																};
+								suma(num1,num2);  
+        break;
+    case "restar":
+        						
+ 								xy();
+
+
+								var resta =function(num1,num2) {
+			
+								return alert(num1-num2);
+
+
+																};
+								resta (num1,num2);
+        break;
+    case "multiplicar":
+        						xy();
+
+
+								var multiplicar =function(num1,num2) {
+			
+								return alert(num1*num2);
+
+
+																};
+								multiplicar (num1,num2);
+        break;
+    case "dividir":
+        						xy();
+								var dividir =function(num1,num2) {
+			
+								return alert(num1/num2);
+
+
+																};
+								dividir (num1,num2);
+        break;
+    
+    default:
+        
+        if(confirm("Eso no existe. Probar de vuelta?"))
+
+    {calculadora();} else {confirm("Vuelva Pronto");};
+       
+};
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
